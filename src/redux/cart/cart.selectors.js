f2039dbdbb836387b462ebbb7bb32b2073b6c75a
Oscar Araya <oscarin821@gmail.com>
2020-01-22 13:01:32 -0600

    Updating App with Reselect
    updating our App with reselect, replaced cart selectors with memoized selectors.

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,17 @@
+import {createSelector} from 'reselect';
+
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+    [selectCart],
+    (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems =>
+        cartItems.reduce(
+            (accummulateQuantity, cartItem) =>
+            accummulateQuantity + cartItem.quantity,
+            0)
+);
