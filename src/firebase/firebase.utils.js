b0d862965c8c40ab3b4066f803d48f70a57dfc82
Oscar Araya <oscarin821@gmail.com>
2020-02-10 19:53:13 -0600

    Moved ShopData into the database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,8 @@ const config = {
     measurementId: "G-CETS44KX8K"
   };
 
+  firebase.initializeApp(config);
+
   export const createUserProfileDocument = async (userAuth, additionalData) => {
     if(!userAuth) return;
 
@@ -23,7 +25,6 @@ const config = {
     if(!snapShot.exists){
       const {displayName, email} = userAuth;
       const createdAt = new Date();
-      
       try{
         await userRef.set({
           displayName,
@@ -39,7 +40,35 @@ const config = {
     return userRef;
   };
 
-  firebase.initializeApp(config);
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+      const newDocRef = collectionRef.doc();
+      batch.set(newDocRef, obj);
+    });
+
+    return await batch.commit();
+  };
+
+  export const convertCollectionsSnapshotToMap = collectionsSnapshot => {
+    const transformedCollection = collectionsSnapshot.docs.map(docSnapshot => {
+      const { title, items } = docSnapshot.data();
+
+      return {
+        routeName: encodeURI(title.toLowerCase()),
+        id: docSnapshot.id,
+        title,
+        items
+      };
+    });
+
+    return transformedCollection.reduce((accumulator, collection) => {
+      accumulator[collection.title.toLowerCase()] = collection;
+      return accumulator;
+    }, {});
+  };
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
