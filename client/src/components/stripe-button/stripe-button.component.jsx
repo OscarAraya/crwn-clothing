6ff058221fefe57a8ca2468c7cc22e0b8026a6df
Oscar Araya <oscarin821@gmail.com>
2020-03-27 14:17:54 -0600

    Redesign of the folder structure
    Redesign of the folder structure and addition of the Node.js.

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishableKey =  'pk_test_00hkk4u68Y83mksy3y16pwKe00tbps2NEv'
+    
+    const onToken = token => {
+
+        console.log(token);
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment successful!')
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert(
+                'There was an issue with your payment. Please sure you use the provided credit card'
+            );
+        })
+    };
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
