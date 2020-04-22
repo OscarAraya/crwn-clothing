e8b42a8f22341e73d460739e5d90d987ffb17945
Oscar Araya <oscarin821@gmail.com>
2020-04-22 17:46:30 -0600

    Updating code to PWA
    Updating our code to be PWA compliant, added serviceWorker, https, updated manifest

--- client/src/components/cart-item/cart-item.component.jsx
+++ client/src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 import './cart-item.styles.scss';
+import { CartItemContainer, ItemDetailsContainer } from './cart-item.styles.jsx';
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
-  <div className='cart-item'>
+  <CartItemContainer>
     <img src={imageUrl} alt='item' />
-    <div className='item-details'>
-      <span className='name'>{name}</span>
-      <span className='price'>
-        {quantity} x ${price}
-      </span>
-    </div>
-  </div>
+      <ItemDetailsContainer>
+          <span className='name'>{name}</span>
+          <span className='price'>
+            {quantity} x ${price}
+          </span>
+        </ItemDetailsContainer>
+    </CartItemContainer>
 );
 
-export default CartItem;
+export default React.memo(CartItem);
